bc3c8e7082ea62dc60da6e9077007e586c0d4188
1549469775 <1549469775@qq.com>
2021-11-03 16:34:58 +0800

    add

---
+++ src/components/Comp1.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{msg: string, show: boolean}>()
+
+</script>
+
+<template>
+  <h1 v-if="show">{{ msg }}</h1>
+</template>
---
+++ src/components/Comp2.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps({
+  msg: String, show: Boolean
+})
+
+</script>
+
+<template>
+  <h1 v-if="show">{{ msg }}</h1>
+</template>
